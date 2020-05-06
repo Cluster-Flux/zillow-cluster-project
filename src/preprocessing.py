3d6ad9ec13cf26db446003a697bb75154cef1ee4
shayaltshue <shayaltshue@gmail.com>
2020-05-06 11:43:14 -0500

    Created new module to handle preprocessing, moved scaling/splitting from prepare.py to new module

---
+++ src/preprocessing.py
@@ -0,0 +1,29 @@
+#####################################
+#  Tools for Splitting and Scaling  #
+#####################################
+
+#--------------- Splitting ---------------#
+def split_data(df):
+    from sklearn.model_selection import train_test_split
+
+    train, test     = train_test_split(df, random_state=123, train_size=.8)
+    train, validate = train_test_split(df, random_state=123, train_size=.8) 
+    
+    return train, test, validate
+
+
+#--------------- Scaling ---------------#
+def numeric_to_object(df, features):    
+    for col in features:
+        df[col] = df[col].astype('object')
+    
+    return df
+
+def scale_data(df):
+    from sklearn.preprocessing import MinMaxScaler
+
+    scaler = MinMaxScaler()
+    numeric_columns = list(df.select_dtypes('number').columns)
+    df[numeric_columns] = scaler.fit_transform(df[numeric_columns])
+
+    return df
