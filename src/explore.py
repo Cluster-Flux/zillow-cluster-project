a7e282d76c4e2a70cdbcd9532b492a0e568808c2
Shay Altshue <shayaltshue@users.noreply.github.com>
2020-05-08 13:34:18 -0500

    final push
    Updated with the final updates

--- src/explore.py
+++ src/explore.py
@@ -10,3 +10,24 @@ def make_coast_graph(ex_df):
     plt.ylabel('Latitude')
     plt.legend()
     plt.show()
+    
+def make_histogram(df):
+    df.hist(figsize=(20, 20), bins = 8, log = False)
+    plt.suptitle('Histograms of the Data')
+    plt.tight_layout
+    
+def make_heatmap(df):
+    corr = df.corr()
+
+    plt.figure(figsize=(20,12))
+    ax = sns.heatmap(
+        corr,
+        #annot = True, <--- Don't run this, it's a nightmare
+        vmin=-1, vmax=1, center=0,
+        cmap=sns.diverging_palette(20, 220, n=200),
+        square=True)
+    
+    ax.set_xticklabels(
+        ax.get_xticklabels(),
+        rotation=45,
+        horizontalalignment='right');
